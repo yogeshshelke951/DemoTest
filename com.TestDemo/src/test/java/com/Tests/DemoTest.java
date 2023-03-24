5d7b0b2e9c370ccd6edde6f10ad13a1342a45c51
ab <ab@aksharbharati>
2023-03-23 21:09:49 -0700

    TestDemo

---
+++ com.TestDemo/src/test/java/com/Tests/DemoTest.java
@@ -0,0 +1,13 @@
+package com.Tests;
+
+import org.testng.annotations.Test;
+
+
+
+public class DemoTest {
+	@Test
+	public void test01() {
+		System.out.println("Hi");
+	}
+
+}
---
+++ com.TestDemo/src/test/java/com/Tests/DemoTest2.java
@@ -0,0 +1,12 @@
+package com.Tests;
+
+import org.testng.annotations.Test;
+
+public class DemoTest2 {
+	@Test
+	public void test01() {
+		System.out.println("hallo");
+		
+	}
+
+}
